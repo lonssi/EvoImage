c7e03df9780589f37456349b400fec7830eac29f
lonssi <laurijrapo@gmail.com>
2018-06-10 14:10:09 +0900

    Handle display forwarding based on platform

--- run-dev-environment.sh
+++ run-dev-environment.sh
@@ -8,7 +8,21 @@ docker build -t evoimage .
 docker-compose down || true
 
 # For display forwarding
-xhost +
+if [ "$(uname)" == "Darwin" ]
+    ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+    xhost + $ip
+    DISPLAY=$ip:0
+then
+   echo MacOS
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
+then
+    echo Linux
+    xhost +
+elif [ -n "$COMSPEC" -a -x "$COMSPEC" ]
+then
+    echo $0: this script does not support Windows \:\(
+    exit
+fi
 
 # Run dev environment
 docker-compose run dev
